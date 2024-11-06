afe94ea15599f2157f48cc4109a58438f0995f4c
Garrett Johnson <pingujohnson@gmail.com>
2024-11-06 03:45:18 -0500

    Setting up

---
+++ main.py
@@ -0,0 +1,25 @@
+# Header Comment
+# Project: [Move Files By Classifier] [https://github.com/GreenBeanio/Move-Files-By-Classifier]
+# Copyright: Copyright (c) [2024]-[2024] [Move Files By Classifier] Contributors
+# Version: [0.1]
+# Status: [Development]
+# License(s): [MIT]
+# Author(s): [Garrett Johnson (GreenBeanio) - https://github.com/greenbeanio]
+# Maintainer: [Garrett Johnson (GreenBeanio) - https://github.com/greenbeanio]
+# Project Description: [This project is used to move files based on a classifier.]
+# File Description: This is the main file for the project.
+
+# Imports
+import pandas
+import argparse
+import sys
+import os
+import pathlib
+import shutil
+import logging
+
+
+
+# Footer Comment
+# History of Contributions:
+# [2024-2024] - [Garrett Johnson (GreenBeanio) - https://github.com/greenbeanio] - [The entire document]
